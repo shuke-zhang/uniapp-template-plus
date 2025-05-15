85e3234066c149fa54d0bf352ea45eedb861601b
shuke <zhangenjian207@gmail.com>
2025-05-15 13:53:36 +0800

    nav-bar 组件优化

--- src/components/nav-bar/nav-bar.vue
+++ src/components/nav-bar/nav-bar.vue
@@ -1,25 +1,29 @@
 <script lang="ts" setup>
 import { NAV_BAR_HEIGHT, getStatusBarHeight } from './nav-bar'
 
+/**
+ * 组件参数定义
+ */
 const props = withDefaults(defineProps<{
   /**
-   * @description 标题
+   * @description 标题文字
    */
   title?: string
+
   /**
-   * @description 是否是从左依次显示
-   * @note customClick 为 true 时 返回icon 按钮在左侧 内容在右侧
+   * @description 标题是否靠左对齐
+   * @note true 表示靠左，false 表示居中
    */
   isLeft?: boolean
+
   /**
    * @description 是否自定义返回事件
-   * @note 如果 isLeft 为 true  则依次从左到右显示
-   * @returns {Event} 返回一个点击事件，开发者可以通过监听该事件来定义自定义的返回行为。
+   * @note 开启后点击返回按钮时不会默认返回上一页，而是触发 click 事件
    */
   customClick?: boolean
+
   /**
-   * @description 是否透明
-   * @note 当透明时 文字和icon 颜色为白色
+   * @description 是否透明样式（如白色字体、透明背景）
    */
   transparent?: boolean
 
@@ -28,23 +32,32 @@ const props = withDefaults(defineProps<{
    */
   showBack?: boolean
 }>(), {
-  // backgroundColor: 'transparent',
   transparent: false,
   customClick: false,
   showBack: true,
 })
 
+/**
+ * 自定义事件
+ */
 const emit = defineEmits(['click'])
 
+/**
+ * 获取状态栏高度，用于顶部内边距
+ */
 const statusBarHeight = ref(getStatusBarHeight())
-const navbarStyle = computed(() => {
-  return {
-    'margin-top': `${statusBarHeight.value}px`,
-    'height': `${NAV_BAR_HEIGHT}px `,
-  }
-})
 
-const slots = useSlots()
+/**
+ * 导航栏样式（高度 + 状态栏高度）
+ */
+const navbarStyle = computed(() => ({
+  'margin-top': `${statusBarHeight.value}px`,
+  'height': `${NAV_BAR_HEIGHT}px`,
+}))
+
+/**
+ * 返回按钮点击事件处理
+ */
 function back() {
   if (props.customClick) {
     emit('click')
@@ -53,130 +66,38 @@ function back() {
     uni.navigateBack({})
   }
 }
-
-const touched = ref(false)
-
-function handleTouchstart() {
-  touched.value = true
-}
-
-function handleTouchend() {
-  touched.value = false
-}
-
-const sysInfo = uni.getWindowInfo()
-const x = sysInfo.windowWidth - 70
-const y = sysInfo.windowHeight - 140
-const showFab = ref(false)
-onMounted(() => {
-  showFab.value = true
-})
-onBeforeUnmount(() => {
-  showFab.value = false
-})
 </script>
 
 <template>
-  <view
-    class="uni-navbar"
-    :class="{ transparent }"
-  >
-    <view
-      class="uni-navbar-inner"
-      :style="navbarStyle"
-    >
-      <view
-        v-if="showBack"
-        class="left-content"
-        @click="back"
-      >
-        <icon-font
-          name="left"
-          :color="transparent ? '#fff' : 'inherit'"
-        />
+  <view class="uni-navbar" :class="{ transparent }">
+    <view class="uni-navbar-inner" :style="navbarStyle">
+      <!-- 左侧区域：返回按钮或插槽 -->
+      <view v-if="showBack" class="left-content" @click="back">
+        <icon-font name="left" :color="transparent ? '#fff' : 'inherit'" />
       </view>
-      <view
-        class="uni-navbar-content"
-        :class="{ 'is-left': isLeft }"
-      >
+      <view v-else class="left-content withe-auto left-content-slot">
+        <slot name="left" />
+      </view>
+
+      <!-- 中间标题区域（默认 slot 或 title 文本） -->
+      <view class="uni-navbar-content" :class="{ 'is-left': isLeft }">
         <slot>{{ title }}</slot>
       </view>
-      <view
-        class="right-content"
-        :color="transparent ? '#fff' : 'inherit'"
-      >
-        <slot
-          name="right"
-        />
+
+      <!-- 右侧插槽区域 -->
+      <view class="right-content">
+        <slot name="right" />
       </view>
     </view>
   </view>
-  <!-- H5 专用浮动按钮 -->
-  <!-- #ifdef H5 -->
-  <Teleport v-if="showFab && slots?.right" to="uni-page-body">
-    <movable-area class="h5-movable-area" :class="{ touched }">
-      <movable-view direction="all" :x="x" :y="y">
-        <view
-          class="movable-area-btn"
-          @touchstart="handleTouchstart"
-          @touchend="handleTouchend"
-        >
-          <slot name="right" />
-        </view>
-      </movable-view>
-    </movable-area>
-  </Teleport>
-  <!-- #endif -->
-
-  <!-- #ifndef H5 -->
-  <movable-area v-if="showFab && slots?.right" class="h5-movable-area" :class="{ touched }" :scale-area="true">
-    <movable-view direction="all" :x="x" :y="y">
-      <view class="movable-area-btn" @touchstart="handleTouchstart" @touchend="handleTouchend">
-        <slot name="right" />
-      </view>
-    </movable-view>
-  </movable-area>
-  <!-- #endif -->
-
-  <!-- <Teleport
-    v-if="showFab && slots?.right "
-    to="uni-page-body"
-  >
-    <movable-area
-      class="h5-movable-area"
-      :class="{ touched }"
-    >
-      <movable-view
-        direction="all"
-        :x="x"
-        :y="y"
-      >
-        <view
-          class="movable-area-btn"
-          @touchstart="handleTouchstart"
-          @touchend="handleTouchend"
-        >
-          <slot
-            name="right"
-          />
-        </view>
-      </movable-view>
-    </movable-area>
-  </Teleport> -->
 </template>
 
 <style lang="scss">
-.uni-icons {
-  font-family: 'UniIconsFontFamily' !important;
-  font-size: 22px;
-  font-style: normal;
-  color: #333;
-}
-
 .uni-navbar {
   border: 1px #eee solid;
   background-color: #fff;
   box-sizing: border-box;
+  z-index: 999;
   &.transparent {
     border-color: transparent;
     background-color: transparent;
@@ -188,13 +109,14 @@ onBeforeUnmount(() => {
 }
 
 .uni-navbar-inner {
-  position: relative;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   box-sizing: border-box;
+  position: relative;
 }
 
+/* 左侧/右侧固定宽度容器 */
 .left-content,
 .right-content {
   display: flex;
@@ -204,6 +126,15 @@ onBeforeUnmount(() => {
   height: 100%;
 }
 
+/* 自定义插槽时左侧宽度自动适配 */
+.withe-auto {
+  width: auto;
+}
+.left-content-slot {
+  padding-left: 15px;
+}
+
+/* 中间内容区绝对定位，占满左右中间空间 */
 .uni-navbar-content {
   position: absolute;
   height: 100%;
@@ -212,41 +143,15 @@ onBeforeUnmount(() => {
   left: 45px;
   right: 45px;
   display: flex;
-  flex-direction: row;
   justify-content: center;
   align-items: center;
+  text-align: center;
 }
 
+/* 标题靠左时应用的样式 */
 .is-left {
   justify-content: flex-start;
-}
-
-.h5-movable-area {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: 99;
-
-  &:not(.touched) {
-    pointer-events: none;
-  }
-}
-
-.movable-area-btn {
-  background-color: $uni-color-primary;
-  width: 100rpx;
-  height: 100rpx;
-  border-radius: 100rpx;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  pointer-events: auto !important;
-
-  > .iconfont {
-    color: #ffffff !important;
-  }
+  text-align: left;
+  padding-left: 10px;
 }
 </style>
